b50b5dc4f7a991a099777df3d24ec54824ba46cf
Wei Zheng <w27zheng@uwaterloo.ca>
2017-11-01 16:23:27 -0400

    added sparsehash version

---
+++ bin/CMakeFiles/manverify.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/manverify.dir/manverify.cxx.o"
+  "manverify.pdb"
+  "manverify"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/manverify.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
