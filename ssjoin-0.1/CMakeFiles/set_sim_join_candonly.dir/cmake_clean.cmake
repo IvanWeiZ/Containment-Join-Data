6590851240df2a310b95a433b56aca488c4f542f
Wei Zheng <zhengw14@mail.cs.toronto.edu>
2017-11-09 20:39:41 -0500

    some changes

---
+++ ssjoin-0.1/CMakeFiles/set_sim_join_candonly.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/set_sim_join_candonly.dir/main.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/input.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/statistics.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/timing.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/cmdline_adaptjoin.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/cmdline_allpairs.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/cmdline_groupjoin.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/cmdline_groupjoin_jaccard.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/cmdline_groupjoin_cosine.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/cmdline_groupjoin_dice.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/cmdline_groupjoin_hamming.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/cmdline_mpjoin.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/cmdline_mpjoin_jaccard.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/cmdline_mpjoin_cosine.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/cmdline_mpjoin_dice.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/cmdline_mpjoin_hamming.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/lsh.cxx.o"
+  "CMakeFiles/set_sim_join_candonly.dir/GitSHA1.cpp.o"
+  "set_sim_join_candonly.pdb"
+  "set_sim_join_candonly"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/set_sim_join_candonly.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
