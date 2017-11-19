80de30cdb7bfc147294addcdb1cc21d139a36255
Wei Zheng <w27zheng@uwaterloo.ca>
2017-11-19 18:47:28 -0500

    prints

--- ssjoin-0.1/similarity.h
+++ ssjoin-0.1/similarity.h
@@ -90,8 +90,8 @@ class CosineSimilarity {
 		inline static unsigned int minsize(unsigned int len, double threshold) {
 			//std::cout<<"len:"<<len<<std::endl;
 			//std::cout<<"minsize:"<<(unsigned int)(ceil(threshold * len))<<std::endl;
-			//return 1;
-			return (unsigned int)(ceil(threshold * len)-1);
+			return 1;
+			//return (unsigned int)(ceil(threshold * len)-1);
 		}
 		
 		inline static unsigned int maxsize(unsigned int len, double threshold, unsigned int maxLen) {
