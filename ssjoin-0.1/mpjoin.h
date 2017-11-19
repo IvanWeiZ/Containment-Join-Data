ab60ad11b81444bd865c50812c0599d52cd18ab5
Wei Zheng <w27zheng@uwaterloo.ca>
2017-11-19 01:40:09 -0500

    123

--- ssjoin-0.1/mpjoin.h
+++ ssjoin-0.1/mpjoin.h
@@ -219,7 +219,7 @@ void MpJoin<MpJoinSimilarity, MpJoinIndexStructurePolicy, MpJoinIndexingStrategy
 		typename Index::ProbeRecord & record = getproberecord(indexedrecords, foreignrecords, recind);
 		unsigned int reclen = record.tokens.size();
 		unsigned int maxLen = indexedrecords[indexedrecords.size() - 1].tokens.size();
-		std::cout << reclen << "   " << indexedrecords[indexedrecords.size() - 1].tokens.size() << std::endl;
+		//std::cout << reclen << "   " << indexedrecords[indexedrecords.size() - 1].tokens.size() << std::endl;
 		//Minimum size of records in index
 		unsigned int minsize = Similarity::minsize(reclen, threshold);
 
