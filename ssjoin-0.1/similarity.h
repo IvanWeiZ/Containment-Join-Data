ecc019e255cb6b0303adccbbec7d065fff918818
Wei Zheng <w27zheng@uwaterloo.ca>
2017-11-21 19:18:35 -0500

    changed maxlen dice

--- ssjoin-0.1/similarity.h
+++ ssjoin-0.1/similarity.h
@@ -124,14 +124,14 @@ class DiceSimilarity {
 		}
 		
 		inline static unsigned int maxsize(unsigned int len, double threshold, unsigned int maxLen) {
-			//std::cout<<len<< " max  "<<maxLen<<" threshold "<<threshold<<std::endl;
+			std::cout<<len<< " max  "<<maxLen<<" threshold "<<threshold<<std::endl;
 			//return len;
 			return (unsigned int)((len / threshold));
 
 		}
 		
 		inline static unsigned int maxsize(unsigned int len, unsigned int pos, double threshold, unsigned int maxLen) {
-			//std::cout<<len<< " pos  max  "<<maxLen<<" threshold "<<threshold<<std::endl;
+			std::cout<<len<< " pos  max  "<<maxLen<<" threshold "<<threshold<<std::endl;
 			return (unsigned int)((len-pos) / threshold);
 		}
 		
